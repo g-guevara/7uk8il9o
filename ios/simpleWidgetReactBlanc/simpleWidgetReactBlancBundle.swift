fd2d662e68c9d7254ddb9a9febde0545717b6ac6
Guillermo Guevara <89048736+g-guevara@users.noreply.github.com>
2025-03-27 13:52:26 -0300

    init

---
+++ ios/simpleWidgetReactBlanc/simpleWidgetReactBlancBundle.swift
@@ -0,0 +1,18 @@
+//
+//  simpleWidgetReactBlancBundle.swift
+//  simpleWidgetReactBlanc
+//
+//  Created by Guillermo Guevara on 27-03-25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct simpleWidgetReactBlancBundle: WidgetBundle {
+    var body: some Widget {
+        simpleWidgetReactBlanc()
+        simpleWidgetReactBlancControl()
+        simpleWidgetReactBlancLiveActivity()
+    }
+}
